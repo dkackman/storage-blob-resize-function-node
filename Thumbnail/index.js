4d27b48624b46fe5a542894050bd9b804dd11cc0
Don Kackman <dkackman@gmail.com>
2022-12-10 09:51:08 -0600

    forked

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -10,22 +10,21 @@ const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 const containerName = process.env.BLOB_CONTAINER_NAME;
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 
-module.exports = async function (context, eventGridEvent, inputBlob){
-    
+module.exports = async function (context, eventGridEvent, inputBlob) {
+
     const blobUrl = context.bindingData.data.url;
-    const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
+    const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
     const widthInPixels = 100;
     Jimp.read(inputBlob).then((thumbnail) => {
 
         thumbnail.resize(widthInPixels, Jimp.AUTO);
 
         thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
-
             const readStream = stream.PassThrough();
             readStream.end(buffer);
 
             const blobClient = new BlockBlobClient(connectionString, containerName, blobName);
-            
+
             try {
                 await blobClient.uploadStream(readStream,
                     uploadOptions.bufferSize,
